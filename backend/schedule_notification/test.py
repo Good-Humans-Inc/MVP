047851da63cb14a54c0128be404fea1db728e7eb
Feng Yan <fengyan06080121@gmail.com>
2025-04-25 21:45:31 -0700

    db update

--- backend/schedule_notification/test.py
+++ backend/schedule_notification/test.py
@@ -1,15 +1,14 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-import datetime
+from datetime import datetime, timezone
 
 cred = credentials.ApplicationDefault()
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
-
-doc_ref = db.collection('users').document('9b11420d-be73-49fd-af1d-bff1ed4f66a0')
+doc_ref = db.collection("users").document("9b11420d-be73-49fd-af1d-bff1ed4f66a0")
 doc_ref.update({
-    'next_notification_time': datetime.datetime.now(datetime.timezone.utc)
+    "next_notification_time": datetime.now(timezone.utc)
 })
 
-print("✅ Firestore document updated with proper event trigger!")
+print("✅ Firestore document updated.")
