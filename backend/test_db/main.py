aeec205e50c8944aaa4e5e680cd81bc637cb19c2
Feng Yan <fengyan06080121@gmail.com>
2025-04-26 13:53:51 -0700

    tests

--- backend/test_db/main.py
+++ backend/test_db/main.py
@@ -1,12 +1,10 @@
-# monitor_user_changes.py
-from firebase_functions import firestore_fn
 from firebase_admin import initialize_app
 
 # Initialize Firebase Admin SDK
 app = initialize_app()
 
-@firestore_fn.on_document_written(document="users/{userId}")
 def monitor_user_changes(data, context):
     print("🚀 Firestore event received!")
     print("Data:", data)
     print("Context:", context)
+
